137d7ee6c3ac0bb75183dbb8603f0f15b2a042d4
hcharlanes <hadriencharlanes@gmail.com>
2017-05-04 13:43:35 -0400

    Activated Doge Mode

--- ens.js
+++ ens.js
@@ -7,14 +7,14 @@ var hash = sha3('salutssasslut');
 
 var words;
 var letters = ['0','1','2','3','4','5','6','7','8','9']
+var lettersTroll = ['wow','such','doge','moon','wow','420','fun','nice','lotz','much']
 var seedWord
 
 function mix2 () {
-	words = [process.argv[2],process.argv[3]]
 	var mixedNames = []
 	for (var i=0; i<300; i++) {
-		var indexWord1 = Math.floor((Math.random() * 8) + 1) -1
-		var indexWord2 = Math.floor((Math.random() * 8) + 1) -1
+		var indexWord1 = Math.floor((Math.random() * 6) + 1) -1
+		var indexWord2 = Math.floor((Math.random() * 6) + 1) -1
 		var lowerWordIndex;
 		var higherWordIndex;
 		if (indexWord2 == indexWord1) {indexWord2++;}
@@ -60,7 +60,7 @@ function mix1 () {
 	var mixedNames = []
 	words = [process.argv[2]]
 	for (var i=0; i<300; i++) {
-		var indexWord = Math.floor((Math.random() * 8) + 1) -1
+		var indexWord = Math.floor((Math.random() * 6) + 1) -1
 			seedWord = [words[0]]
 		for (var j=0; j<indexWord; j++) {
 			seedWord.splice(0,0,letters[Math.floor((Math.random() * 10) + 1) -1])
@@ -86,10 +86,19 @@ function timeLeftBool (string) {
 	else return false
 }
 
-
-
-if (process.argv[3] == 'undefined') {
-	mix1()
+if (process.argv[2] == 'doge') {
+	words = [process.argv[3],process.argv[4]]
+	letters = lettersTroll;
+	if (process.argv[4] == 'undefined') {
+		mix1()
+	}
+	else (mix2())	
 }
-else (mix2())
 
+else {
+	words = [process.argv[2],process.argv[3]]
+	if (process.argv[3] == 'undefined') {
+		mix1()
+	}
+	else (mix2())
+}
